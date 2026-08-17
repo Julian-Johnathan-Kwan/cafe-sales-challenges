64965fe1466ad42fe991d12be6c976895e42d74b
Julian-Johnathan-Kwan <YuiJulian.Kwan@my.mrc.tas.edu.au>
2026-08-17 02:39:36 +0000

    awesomemenu

--- 01-cafe-pos/tier4_challenging/pos_system/menu.py
+++ 01-cafe-pos/tier4_challenging/pos_system/menu.py
@@ -26,7 +26,7 @@ import os
 class Menu:
     """Loads and provides access to the café's menu."""
 
-    def __init__(self, data_path="menu_data.json"):
+    def __init__(self, data_path="/workspaces/cafe-sales-challenges/01-cafe-pos/tier4_challenging/pos_system/menu_data.json"):
         """
         Load the menu from the JSON file at data_path into self.items,
         a dictionary of {item_name: price}.
@@ -36,21 +36,32 @@ class Menu:
                 self.items = json.load(f)
         """
         # TODO: implement this method
+        with open( data_path, "r") as f:
+            self.items = json.load(f)
         pass
 
     def has_item(self, item_name):
         """Return True if item_name exists on the menu, False otherwise."""
         # TODO: implement this method
+        if item_name in self.items:
+            return True
+        else:
+            return False
         pass
 
     def get_price(self, item_name):
         """Return the price of item_name, or None if it isn't on the menu."""
         # TODO: implement this method
+        if self.has_item(item_name):
+            return self.items[item_name]
+        else:
+            return None
         pass
 
     def list_items(self):
         """Return a list of every item name on the menu."""
         # TODO: implement this method
+        
         pass
 
 
